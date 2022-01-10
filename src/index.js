3647af42bb203c50ad4dacafbd7aa65703e2343e
Sergey <88570614+SergeyGutman@users.noreply.github.com>
2022-01-10 15:50:32 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function toReadable (number) {
-  
+     let simpleUnits = ['', 'one', 'two', 'three', 'four', 'five' ,'six' ,'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen',
+    'nineteen'];
+    let complexUnits = ['','', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    if (number <= 19) return console.log(simpleUnits[number]);
+    else if (number >= 20 && number < 100){
+        let kres = String(number);
+        return console.log(complexUnits[kres[0]] + ' ' + simpleUnits[kres[1]]);
+    }
 }
