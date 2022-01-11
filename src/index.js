871f5c008a8be3d5a243abfebe8a884d607cc0cd
sergutman <sergutman4@gmail.com>
2022-01-11 19:44:40 +0300

    fix: fixed task solution

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 module.exports = function toReadable (number) {
-     let simpleUnits = ['', 'one', 'two', 'three', 'four', 'five' ,'six' ,'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen',
-    'nineteen'];
-    let complexUnits = ['','', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    if (number == 0) return 'zero';
-    if (number <= 19) return simpleUnits[number];
-    else if (number >= 20 && number < 100){
-        let kres = String(number);
-        let sum = complexUnits[kres[0]] + ' ' + simpleUnits[kres[1]];
-        return sum;
-    }
-    else if (number >= 100){
-        let hideDecimal = '';
-        let kres = String(number);
-        if (kres[1] != 0) hideDecimals = complexUnits[kres[1]] + ' ';
-        let sum = simpleUnits[kres[0]] + ' ' + 'hundred ' + hideDecimals + simpleUnits[kres[2]];
-        return sum;
-    }
+    let simpleUnits = ['', 'one', 'two', 'three', 'four', 'five' ,'six' ,'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let decimalsUnits = ['','', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+       if (number == 0) return 'zero';
+       if (number <= 19) return simpleUnits[number];
+       if (number >= 20 && number < 100){
+           return (decimalsUnits[Math.trunc(number/10)] + ' ' + simpleUnits[number%10]).trim();
+       }
+    let countHundred = Math.trunc(number/100);
+    let outputHundred = '';
+    let outputDecimals = '';
+       if (countHundred >= 1){
+               outputHundred = simpleUnits[countHundred] + ' hundred';
+               let remainder = number%100;
+               if (remainder <= 19){
+                   outputDecimals = ' ' + simpleUnits[remainder];
+               }
+               else {
+                   outputDecimals = ' ' + decimalsUnits[Math.trunc(remainder/10)] + ' ' + simpleUnits[remainder%10];
+               }
+               let result = outputHundred + outputDecimals;
+       return result.trim();
+       }
 }
